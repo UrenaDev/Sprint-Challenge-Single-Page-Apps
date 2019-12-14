ed0b0df59ceb49d7f3af78000a8fb8f0e8507a52
Ben Urena <urenadev@gmail.com>
2019-12-13 20:46:55 -0600

    got characters displaying from api. displaying name element for each

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  const { name, image, id } = character;
+  return (
+    <div className='characterCard' key={id}>
+      <img src= {image} />
+      <h2> {name} </h2>
+    </div>
+  );
 }
